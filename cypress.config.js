1d028c2d6f211f4fa85d586487a6b277a37440a1
DESKTOP-T6FL8DH\Thanh <thanh.dao@eastgate-software.com>
2022-11-03 15:28:26 +0700

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'qx1r27',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ cypress/e2e/Amatrium/AmtriumElements.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" /> 
+
+export default class AmatriumElements {
+
+    static PrivacyPolicyOpenButton = '.chakra-stack > :nth-child(1)' ;  
+    static PrivacyPolicyCloseButton = '.chakra-modal__close-btn' ; 
+    static TermOpenButton = '#root > div > div > div > div.chakra-stack.css-1dhz0x9 > a:nth-child(2)' ; 
+    static TermCloseButton = '.chakra-modal__close-btn' ;
+    static EmailAddressField = '#field-2'; 
+    static PassWordField = '#password'; 
+    static PassWordHideButton = '.chakra-input__right-element > .chakra-button';
+    static PrivacyPolicyPanelHeader = '#chakra-modal--header-5';
+    static Loginbutton = 'button[class="chakra-button css-mx6vab"]'; 
+}; 
+
