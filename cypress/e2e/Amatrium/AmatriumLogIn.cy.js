7d562c7695ff956bd72c6c2de838c82590615e90
DESKTOP-T6FL8DH\Thanh <thanh.dao@eastgate-software.com>
2022-11-07 16:57:23 +0700

    Fix variable naming convention in AmatriumElements.js Fix element locator in AmatriumElements.js

--- cypress/e2e/Amatrium/AmatriumLogIn.cy.js
+++ cypress/e2e/Amatrium/AmatriumLogIn.cy.js
@@ -1,86 +1,82 @@
 /// <reference types="cypress" /> 
 
 
-import  AmatriumElements  from './AmtriumElements.js';
+import AmatriumElements from './AmtriumElements.js';
 
 context('Amatrium Main Menu test matrix', () => {
     before(() => {
-      cy.visit('dev.amatrium.com')
+        cy.visit('dev.amatrium.com')
     });
 
     describe('Amatrium Main Menu test matrix', () => {
 
-it('It will navigate to Privacy Policy', () => { 
-    expect(AmatriumElements.PrivacyPolicyOpenButton, 'Privacy Policy Open button should be available').to.exist
-    cy.get(AmatriumElements.PrivacyPolicyOpenButton).click()
-}
-) ;
+        it('It will navigate to Privacy Policy', () => {
+            expect(AmatriumElements.privacyPolicyOpenButton, 'Privacy Policy Open button should be available').to.exist
+            cy.get(AmatriumElements.privacyPolicyOpenButton).click()
+        }
+        );
 
-it('It will check Privacy Policy panel ', () => { 
-    
-    cy.get(AmatriumElements.PrivacyPolicyPanelHeader).should('have.text','Privacy Policy').click()
-}
-) ;
+        it('It will check Privacy Policy panel ', () => {
 
-it('It will close Privacy Policy', () => { 
-    expect(AmatriumElements.PrivacyPolicyCloseButton, 'Privacy Policy Close button should be available').to.exist
-    cy.get(AmatriumElements.PrivacyPolicyCloseButton).click()
-}
-) ;
+            cy.get(AmatriumElements.privacyPolicyPanelHeader).should('have.text', 'Privacy Policy').click()
+        }
+        );
 
-it('It will open Term', () => { 
-    expect(AmatriumElements.TermOpenButton, 'Term Open button should be available').to.exist
-    cy.contains('Terms').click()
-    //cy.get(AmatriumElements.TermOpenButton).click()
-}
-) ;
- 
-it('It will check Term panel ', () => { 
-    
-    cy.get('#chakra-modal--header-5').should('have.text','Terms of Service')
-}
-) ;
+        it('It will close Privacy Policy', () => {
+            expect(AmatriumElements.privacyPolicyCloseButton, 'Privacy Policy Close button should be available').to.exist
+            cy.get(AmatriumElements.privacyPolicyCloseButton).click()
+        }
+        );
 
-it('It will close Term', () => { 
-    expect(AmatriumElements.TermCloseButton, 'Term Close button should be available').to.exist
-    cy.get(AmatriumElements.TermCloseButton).click()
-}
-) ;
+        it('It will open Term', () => {
+            expect(AmatriumElements.termOpenButton, 'Term Open button should be available').to.exist
+            cy.contains('Terms').click()
+            //cy.get(AmatriumElements.TermOpenButton).click()
+        }
+        );
 
-it('It will input email in Email Address field', () => { 
-    cy.get(AmatriumElements.EmailAddressField)
-    .type("britishcolumbia247@gmail.com", {timeout: 5000}).should('have.value','britishcolumbia247@gmail.com')
+        it('It will check Term panel ', () => {
 
-}
-) ;
+            cy.get('#chakra-modal--header-5').should('have.text', 'Terms of Service')
+        }
+        );
 
-it('It will input password in Password field', () => { 
-    cy.get(AmatriumElements.PassWordField).type("w7ekg2w", {timeout: 5000}).should('have.value','w7ekg2w')
+        it('It will close Term', () => {
+            expect(AmatriumElements.termCloseButton, 'Term Close button should be available').to.exist
+            cy.get(AmatriumElements.termCloseButton).click()
+        }
+        );
 
-    
-}
-) ;
+        it('It will input email in Email Address field', () => {
+            cy.get(AmatriumElements.emailAddressField)
+                .type("britishcolumbia247@gmail.com").should('have.value', 'britishcolumbia247@gmail.com')
 
-it('It will unhide password in Password field', () => { 
-    cy.get(AmatriumElements.PassWordHideButton).should('exist')
-    cy.get(AmatriumElements.PassWordHideButton).click()
-    
-}
-) ;
+        }
+        );
 
-it('It will rehide password in Password field', () => { 
-    cy.get(AmatriumElements.PassWordHideButton).should('exist')
-    cy.get(AmatriumElements.PassWordHideButton).click()
-    
-}
-) ;
+        it('It will input password in Password field', () => {
+            cy.get(AmatriumElements.passWordField).type("w7ekg2w").should('have.value', 'w7ekg2w')
 
-it('It will check Login button', () => { 
-    cy.get(AmatriumElements.Loginbutton).should('exist').should('have.text','Login')
-    cy.get(AmatriumElements.Loginbutton,{timeout: 6000}).click()
-    
-}
-) ;
 
-})
+        }
+        );
+
+        it('It will unhide password in Password field', () => {
+            cy.get(AmatriumElements.passWordHideButton).should('exist')
+            cy.get(AmatriumElements.passWordHideButton).click()
+
+            cy.get(AmatriumElements.passWordHideButton).click()
+        }
+        );
+
+       
+
+        it('It will check Login button', () => {
+            cy.get(AmatriumElements.loginbutton).should('exist').should('have.text', 'Login')
+            cy.get(AmatriumElements.loginbutton).click()
+
+        }
+        );
+        
+    })
 });
--- cypress/e2e/Amatrium/AmtriumElements.js
+++ cypress/e2e/Amatrium/AmtriumElements.js
@@ -2,14 +2,14 @@
 
 export default class AmatriumElements {
 
-    static PrivacyPolicyOpenButton = '.chakra-stack > :nth-child(1)' ;  
-    static PrivacyPolicyCloseButton = '.chakra-modal__close-btn' ; 
-    static TermOpenButton = '#root > div > div > div > div.chakra-stack.css-1dhz0x9 > a:nth-child(2)' ; 
-    static TermCloseButton = '.chakra-modal__close-btn' ;
-    static EmailAddressField = '#field-2'; 
-    static PassWordField = '#password'; 
-    static PassWordHideButton = '.chakra-input__right-element > .chakra-button';
-    static PrivacyPolicyPanelHeader = '#chakra-modal--header-5';
-    static Loginbutton = 'button[class="chakra-button css-mx6vab"]'; 
-}; 
+    static privacyPolicyOpenButton = '.chakra-stack > :nth-child(1)';
+    static privacyPolicyCloseButton = '.chakra-modal__close-btn';
+    static termOpenButton = '//*[text()[contains(., "Terms")]]';
+    static termCloseButton = '.chakra-modal__close-btn';
+    static emailAddressField = '#field-2';
+    static passWordField = '#password';
+    static passWordHideButton = '.chakra-input__right-element > .chakra-button';
+    static privacyPolicyPanelHeader = '#chakra-modal--header-5';
+    static loginbutton = 'form > button.chakra-button';
+};
 
