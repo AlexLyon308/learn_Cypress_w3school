eadd5a18a48405b8204fad076afadfb0ec0f3ffd
DESKTOP-T6FL8DH\Thanh <thanh.dao@eastgate-software.com>
2022-11-09 15:03:25 +0700

    Update multiple assertion in Test_AmatriumLogin.cy.js

--- cypress/e2e/Amatrium/AmtriumElements.js
+++ cypress/e2e/Amatrium/AmtriumElements.js
@@ -2,6 +2,7 @@
 
 export default class AmatriumElements {
 
+    //Login screen elements
     static privacyPolicyOpenButton = '.chakra-stack > :nth-child(1)';
     static privacyPolicyCloseButton = '.chakra-modal__close-btn';
     static termOpenButton = '//*[text()[contains(., "Terms")]]';
@@ -11,5 +12,7 @@ export default class AmatriumElements {
     static passWordHideButton = '.chakra-input__right-element > .chakra-button';
     static privacyPolicyPanelHeader = '#chakra-modal--header-5';
     static loginbutton = 'form > button.chakra-button';
+
+    //Main screen elements 
 };
 
