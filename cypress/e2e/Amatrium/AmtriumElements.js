b36a6039feccb78cda2600583cbdfa36a8c32e97
DESKTOP-T6FL8DH\Thanh <thanh.dao@eastgate-software.com>
2022-11-12 10:17:00 +0700

    update termOpenButton

--- cypress/e2e/Amatrium/AmtriumElements.js
+++ cypress/e2e/Amatrium/AmtriumElements.js
@@ -5,7 +5,7 @@ export default class AmatriumElements {
     //Login screen elements
     static privacyPolicyOpenButton = '.chakra-stack > :nth-child(1)';
     static privacyPolicyCloseButton = '.chakra-modal__close-btn';
-    static termOpenButton = 'chakra-modal--header-5';
+    static termOpenButton = '.chakra-stack > :nth-child(2)';
     static termCloseButton = '.chakra-modal__close-btn';
     static emailAddressField = '#field-2';
     static passWordField = '#password';
