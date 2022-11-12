23ed16480aa2f87356d092e4bbc744230a880cf7
DESKTOP-T6FL8DH\Thanh <thanh.dao@eastgate-software.com>
2022-11-12 11:39:29 +0700

    add new test

--- cypress/e2e/Amatrium/AmtriumElements.js
+++ cypress/e2e/Amatrium/AmtriumElements.js
@@ -23,14 +23,14 @@ export default class AmatriumElements {
     static mainScreen_CompanyButton 
     static mainScreen_SearchButton 
     static mainScreen_SearchBox 
-    static mainScreen_UnitConversionTable 
+    static mainScreen_UnitConversionTable_Button = '#chakra-modal--body-70 > div:nth-child(1) > video' ;
     static mainScreen_SendYourSuggestion 
     static mainScreen_AskAmatriumExpert 
     static mainScreen_ChatBot 
     static mainScreen_MaterialSearchHeader
     static mainScreen_LogOutButton
-    static mainScreen_HowItWorks 
-    static mainScreen_HowItWorks_Tooltips 
-    static mainScreen_HowItWorks_IntroductionVideo
+    static mainScreen_HowItWorks = 'button[class="chakra-button css-1op0i7"]'; 
+    static mainScreen_HowItWorks_Tooltips = '.introjs-tooltip-title'; 
+    static mainScreen_HowItWorks_IntroductionVideo = '#chakra-modal--body-70 > div:nth-child(1) > video';
 };
 
